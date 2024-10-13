22b02876e9f6a3d0a79ac0696944743d67b5f92a
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-10-13 16:13:03 -0400

    feat: adds react v18 (#3)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,32 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  typescript: {
-    parser: '@typescript-eslint/parser',
-    extends: [
-      'plugin:@typescript-eslint/recommended', // TypeScript rules
-    ],
+export default [
+  ...antfu(),
+  {
+    files: ['*.tsx', '*.ts'],
+    plugins: {
+      react: require('eslint-plugin-react'),
+      'react-hooks': require('eslint-plugin-react-hooks'),
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
     rules: {
-      // Add TypeScript-specific rules here
+      // Add React-specific rules here if needed
+      'react/jsx-uses-react': 'off', // Not needed for React 17+ with new JSX transform
+      'react/react-in-jsx-scope': 'off', // Not needed for React 17+ with new JSX transform
+      'react-hooks/rules-of-hooks': 'error', // Enforce the rules of Hooks
+      'react-hooks/exhaustive-deps': 'warn', // Warn about missing dependencies in useEffect
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-})
+]
+
