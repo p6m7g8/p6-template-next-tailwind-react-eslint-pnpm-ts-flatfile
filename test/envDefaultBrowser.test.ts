e8506b3fa7d7eb93e9788aefb42251ffbd2dcd42
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-10-14 10:39:25 -0400

    feat: tailwind&postcss

--- test/envDefaultBrowser.test.ts
+++ test/envDefaultBrowser.test.ts
@@ -1,11 +1,7 @@
-import * as dotenv from 'dotenv'
 import { REACT_APP_JEST_TEST_VARIABLE } from '../src/dotenv'
-
-// Load the .env.default file
-dotenv.config({ path: '.env.default' })
 
 describe('react Browser Environment - Default Environment Variables', () => {
   it('should load REACT_APP_JEST_TEST_VARIABLE from .env.default', () => {
-    expect(REACT_APP_JEST_TEST_VARIABLE).toBe('21')
+    expect(REACT_APP_JEST_TEST_VARIABLE).toBe('t4')
   })
 })
--- test/envTestBrowser.test.ts
+++ test/envTestBrowser.test.ts
@@ -1,11 +1,7 @@
-import * as dotenv from 'dotenv'
 import { REACT_APP_JEST_TEST_IN_TEST } from '../src/dotenv'
-
-// Load the .env.test file
-dotenv.config({ path: '.env.test' })
 
 describe('react Browser Environment - Test Environment Variables', () => {
   it('should load REACT_APP_JEST_TEST_IN_TEST from .env.test', () => {
-    expect(REACT_APP_JEST_TEST_IN_TEST).toBe('15.5')
+    expect(REACT_APP_JEST_TEST_IN_TEST).toBe('t3')
   })
 })
